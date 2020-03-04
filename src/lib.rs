98b6b8accbd5ab1c6020333e3c1fc1fd0a1babd8
lipu <versee97@gmail.com>
2020-03-04 17:46:32 +0800

    小修复

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ pub fn generate_random_sequence(keys_tones: &HashMap<usize, &&str>) {
     let keys_tones_length = keys_tones.len();
     let mut nums: Vec<usize> = (0..keys_tones_length).collect();
 
-    for i in 1..25 {
+    for i in 1..46 {
         for j in 0..keys_tones_length {
             let secret_number = rand::thread_rng().gen_range(0, keys_tones_length);
             nums.swap(j, secret_number);
 
         }
         for t in &nums {
-            print!("{} ", keys_tones.get(t).unwrap());
+            print!("{}", keys_tones.get(t).unwrap());
         }
         if i%3==0 {
             println!();
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use fifty_tone::generate_random_sequence;
 use std::collections::HashMap;
 
 fn main() {
-
     let fifty_tones = vec!["あ", "い", "う", "え", "お"];
     let keys_tones: HashMap<usize, &&str> = (0..fifty_tones.len()).zip(fifty_tones.iter()).collect();
     generate_random_sequence(&keys_tones);
